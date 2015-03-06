0707dc8e6ee4705764e744b14d32ffa4a87999d1
haoc <haoc92@gmail.com>
2015-03-05 18:13:13 -0600

    more formatting

--- EvilHangman/public/assets/main.js
+++ EvilHangman/public/assets/main.js
@@ -48,3 +48,24 @@ function confirmation() {
     }
     
 }
+
+
+
+// Only works if it's in .html; NEED TO FIX
+//        $(document).ready(function() {
+//    $("#numbers_only").keydown(function (e) {
+//        // Allow: backspace, delete, escape, enter.
+//        if ($.inArray(e.keyCode, [46, 8, 27, 13, 110]) !== -1 ||
+//             // Allow: Ctrl+A
+//            (e.keyCode == 65 && e.ctrlKey === true) || 
+//             // Allow: home, end, left, right
+//            (e.keyCode >= 35 && e.keyCode <= 39)) {
+//                 // let it happen, don't do anything
+//                 return;
+//        }
+//        // Ensure that it is a number and stop the keypress
+//        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+//            e.preventDefault();
+//        }
+//    });
+//});
